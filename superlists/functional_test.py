8457fd254634611cc12ffa4a93e652ab9af308e9
incheol77 <incheol77@gmail.com>
2019-12-22 21:27:17 +0900

    Initial commit

---
+++ superlists/functional_test.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_latr(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'), 
+				'input op item'
+		)
+		inputbox.send_keys('fraud')
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+				any(row.text == '1: fraud' for row in rows),
+		)
+		self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+	
+
